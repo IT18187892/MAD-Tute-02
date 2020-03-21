5aaf6ded6100c2b78ba32e59bef963ed19bf3989
Jayawardana JTH <tiran2323@gmail.com>
2020-03-21 21:04:56 +0530

    add functions to ok button

--- IntentsProj/app/src/main/java/com/example/intentsproj/FirstActivity.java
+++ IntentsProj/app/src/main/java/com/example/intentsproj/FirstActivity.java
@@ -2,13 +2,35 @@ package com.example.intentsproj;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class FirstActivity extends AppCompatActivity {
+
+    EditText number1;
+    EditText number2;
+    Button btn_ok;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
+
+        number1 = findViewById(R.id.id_input1);
+        number2 = findViewById(R.id.id_input2);
+        btn_ok = findViewById(R.id.id_btn_ok);
+
+        btn_ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FirstActivity.this,SecondActivity.class);
+
+
+
+            }
+        });
     }
 }
